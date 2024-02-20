c4948f70400cfc624c1683423dce8f51a739c9d1
Byakuya91 <nganti91@yahoo.com>
2024-02-20 16:14:30 -0500

    I've managed to test out the dictionary API endpoint. The next step is constructing a method to take in an input and create a function that will store the word and pass it down as props through header to the searchBar component I have.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
+// ? React imports
 import { useState } from "react";
+// ? Component imports
+import Header from "./Components/Dic_Header/Header/Header";
+// ? module imports
+import axios from "axios";
+
+// TODO:
+// 1) research API and how it works(ONGOING)
+// 2) Implement and test the API(ONGOING)
+// 3) pass down the Async function down to the search bar component.
+// 4) Hook up the search bar(after setting it up) and have it make an API
 
 function App() {
+  // ? STEP ONE: define state variables
+
+  // ? Storing fetch API data
+  const [data, setData] = useState(null);
+  // ? Track an error during API data fetching
+  const [error, setError] = useState(false);
+  // ? Variable to track is there's an input error
+  const [inputerror, setInputError] = useState(false);
+  // ? toggles between 'light' and 'dark' themes
+  const [toggleTheme, setToggleTheme] = useState(true);
+  // ? stores selected font
+  const [font, setFont] = useState("inter");
+
+  // ? STEP TWO: API to dictionary
+  // Testing the dictionary API call
+  axios
+    .get("https://api.dictionaryapi.dev/api/v2/entries/en/augment")
+    .then((response) => {
+      console.log("the word is: ", response.data[0]); // Log the response data to the console
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error); // Log any errors to the console
+    });
+
   return (
     <div>
-      <p>Hello World</p>
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Dic_Header/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <p>Header placement text</p>
+    </div>
+  );
+};
+
+export default Header;
