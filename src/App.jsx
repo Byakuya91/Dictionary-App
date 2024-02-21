c87e10d70b9451cfe5632d8fddf8d4f5ae43e982
Byakuya91 <nganti91@yahoo.com>
2024-02-20 19:56:49 -0500

    I've finished coding out the helper functions and figured out the CSS sheet for the app.js. The next step is coding out the searchBar and getting that searchBar functional to make the API call. Afterwards I will focus on displaying the word before rounding out the font and toggle.

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ import Header from "./Components/Dic_Header/Header/Header";
 import axios from "axios";
 
 // TODO:
-// 1) research API and how it works(ONGOING)
-// 2) Implement and test the API(ONGOING)
+// 1) research API and how it works(DONE)
+// 2) Implement and test the API(DONE)
 // 3) pass down the Async function down to the search bar component.
-// 4) Hook up the search bar(after setting it up) and have it make an API
+// 4) Code out helper functions and flesh out CSS sheet.
+// 5) Hook up the search bar(after setting it up) and have it make an API
 
 function App() {
   // ? STEP ONE: define state variables
@@ -19,27 +20,80 @@ function App() {
   // ? Track an error during API data fetching
   const [error, setError] = useState(false);
   // ? Variable to track is there's an input error
-  const [inputerror, setInputError] = useState(false);
+  const [inputError, setInputError] = useState(false);
   // ? toggles between 'light' and 'dark' themes
   const [toggleTheme, setToggleTheme] = useState(true);
   // ? stores selected font
   const [font, setFont] = useState("inter");
 
   // ? STEP TWO: API to dictionary
-  // Testing the dictionary API call
-  axios
-    .get("https://api.dictionaryapi.dev/api/v2/entries/en/augment")
-    .then((response) => {
-      console.log("the word is: ", response.data[0]); // Log the response data to the console
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error); // Log any errors to the console
-    });
+
+  // ?SUB STEP ONETesting the dictionary API call(TEST COMPLETED)
+  // axios
+  //   .get("https://api.dictionaryapi.dev/api/v2/entries/en/augment")
+  //   .then((response) => {
+  //     console.log("the word is: ", response.data[0]); // Log the response data to the console
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error fetching data:", error); // Log any errors to the console
+  //   });
+
+  // ? SUB STEP TWO: creating function for axios call
+  const grabWord = async () => {
+    //  ? resetting the errors for Input and API fetching
+    setError(false);
+    setInputError(false);
+
+    try {
+      // ?If there's NO input
+      if (input === "") {
+        // ? Set input error if input is empty
+        setInputError(true);
+        return;
+      }
+
+      // ! Making the API call
+      const response = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${input}`
+      );
+
+      // ? Converting response data to JSON
+      const responseData = response.data;
+
+      //  ? setting the fetched data
+      setData(responseData);
+    } catch (error) {
+      // If the data is NOT retrieved
+      setError(true);
+    }
+  };
+
+  // ?SUB-STEP THREE: Helper functions
+
+  // ? dark and light mode function
+  const handleToggle = () => {
+    // Flipping toggle boolean
+    setToggleTheme((prevToggleState) => !prevToggleState);
+  };
+
+  // ? font changing
+  const handleFontChange = () => {
+    setFont(font);
+  };
 
   return (
-    <div>
-      <Header />
-    </div>
+    <main className="`${toggleTheme ? 'light' : 'dark'} ${font}`">
+      <div className="container">
+        <Header
+          grabWord={grabWord}
+          toggle={toggleTheme}
+          handleToggle={handleToggle}
+          font={font}
+          handleFontChange={handleFontChange}
+          error={inputError}
+        />
+      </div>
+    </main>
   );
 }
 
